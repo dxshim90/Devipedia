996d4fb0959511a7f7f556457fe6d3227cd143d0
github username <github email address>
2019-05-15 13:55:40 +0100

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useNewUrlParser: true
+      }
+    );
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(err.message);
+  }
+};
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
